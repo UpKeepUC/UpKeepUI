a0ad343ef5782aa58af331c31a8119d065cbedd1
handbrennan <handbd@mail.uc.edu>
2023-04-10 21:39:03 -0400

    prettier code format changes

--- src/components/common/BasicSnackbar/BasicSnackbar.jsx
+++ src/components/common/BasicSnackbar/BasicSnackbar.jsx
@@ -1,26 +1,19 @@
-import React, { forwardRef } from 'react';
-import Snackbar from '@mui/material/Snackbar';
-import MuiAlert from '@mui/material/Alert';
+import React, { forwardRef } from "react";
+import Snackbar from "@mui/material/Snackbar";
+import MuiAlert from "@mui/material/Alert";
 
 const Alert = forwardRef(function Alert(props, ref) {
   return <MuiAlert elevation={6} ref={ref} variant="filled" {...props} />;
 });
 
 const BasicSnackbar = ({ open, onClose, severity, message }) => {
-    return (
+  return (
     <>
-        <Snackbar
-            open={open}
-            autoHideDuration={6000}
-            onClose={onClose}
-        >
-            <Alert
-                onClose={onClose}
-                severity={severity}
-            >
-                {message}
-            </Alert>
-        </Snackbar>
+      <Snackbar open={open} autoHideDuration={6000} onClose={onClose}>
+        <Alert onClose={onClose} severity={severity}>
+          {message}
+        </Alert>
+      </Snackbar>
     </>
   );
 };
